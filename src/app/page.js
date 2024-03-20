6287dec6499757dd3139fa895f7a1a3e4407acb9
Alexig2 <alex4342on@gmail.com>
2024-03-20 17:32:44 -0400

    Aula dia 20/03

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
-
 export default async function Home() {
+
+  const resposta = await fetch("https://back-end-ifms.vercel.app/campi", {
+    next: {
+      revalidate: 1
+    }
+  });
+  const campi = await resposta.json();
 
   return (
     <main>
-      <h1>Página inicial</h1>
-      <h1>Em desenvolvimento</h1>
+      <h1>Página inicialaa</h1>
+      {
+        campi.map((campus) =>
+        <div>
+          <p>{campus.nome_campus}</p>
+        </div>
+        )   
+      }
     </main>
   )
 }
